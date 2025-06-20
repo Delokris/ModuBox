80a896c1a0afa96adcdc4bfd8fe335874d6fdf68
Julian Linden <DelokrisGaming@web.de>
2025-06-20 06:49:38 +0000

    Added image css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 function buttonCount(){
     count = count+1
+    console.log("Button clicked " + count + " times");
 }
-let count =0
+let count = 0;
 buttonCount();
+let shoppingList = [];
+function addToShoppingList(itemID) {
+    shoppingList.push(item);
+    console.log("Item added to shopping list: " + item);
+    console.log("Current shopping list: " + shoppingList.join(", "));
+}
