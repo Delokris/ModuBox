55499f18187d43156e956c56d117c85d260d6f7d
Julian Linden <DelokrisGaming@web.de>
2025-06-23 10:44:40 +0000

    Added Sidebar to other html forms

--- js/script.js
+++ js/script.js
@@ -19,6 +19,12 @@ function adjustSize() {
     document.getElementById('home_big').style.width = document.defaultView.getComputedStyle(document.getElementById('home_small'), "").getPropertyValue("width");
     document.getElementById('shop_big').style.width = document.defaultView.getComputedStyle(document.getElementById('shop_small'), "").getPropertyValue("width");
     document.getElementById('about_big').style.width = document.defaultView.getComputedStyle(document.getElementById('about_small'), "").getPropertyValue("width");
+    document.getElementById('container-impressum').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('container-impressum').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
+    document.getElementById('container-contact').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('container-contact').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
+    document.getElementById('container-privacy').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('container-privacy').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
     
 
 }
