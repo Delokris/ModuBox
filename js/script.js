4e0365f5b6c12fdadf67b9c0ee7342201be64dc8
Julian Linden <DelokrisGaming@web.de>
2025-06-23 09:18:38 +0000

    Fixed height using js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ function addToShoppingList(itemID) {
     console.log("Item added to shopping list: " + item);
     console.log("Current shopping list: " + shoppingList.join(", "));
 }
+function adjustHeight() {
+    document.getElementById('left-hotbar').style.height = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('left-container').style.height = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('right-container').style.height = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+}
+
+
+adjustHeight();
