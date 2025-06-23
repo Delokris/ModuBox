dc277cc171cdb75f7ac70946cfdcf33a86dfc6ce
Julian Linden <delokrisgaming@web.de>
2025-06-23 21:41:31 +0200

    Added Hotbar to all pages

--- js/script.js
+++ js/script.js
@@ -13,20 +13,14 @@ function addToShoppingList(itemID) {
 function adjustSize() {
     document.getElementById('left-hotbar').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('left-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('center-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('right-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('center-container').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
+    document.getElementById('center-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('home_big').style.width = document.defaultView.getComputedStyle(document.getElementById('home_small'), "").getPropertyValue("width");
     document.getElementById('shop_big').style.width = document.defaultView.getComputedStyle(document.getElementById('shop_small'), "").getPropertyValue("width");
     document.getElementById('about_big').style.width = document.defaultView.getComputedStyle(document.getElementById('about_small'), "").getPropertyValue("width");
     document.getElementById('container-impressum').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('container-impressum').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
     document.getElementById('container-contact').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('container-contact').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
     document.getElementById('container-privacy').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('container-privacy').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
-    
-
 }
 
 adjustSize();
