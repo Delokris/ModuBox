1d22a57e8902ab5f107570883adbba4f7d559dfe
Julian Linden <DelokrisGaming@web.de>
2025-06-23 09:47:43 +0000

    Fixed width and removed useless subdivision

--- js/script.js
+++ js/script.js
@@ -10,10 +10,14 @@ function addToShoppingList(itemID) {
     console.log("Item added to shopping list: " + item);
     console.log("Current shopping list: " + shoppingList.join(", "));
 }
-function adjustHeight() {
+function adjustSize() {
     document.getElementById('left-hotbar').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('left-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('center-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('right-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('center-container').style.left = document.defaultView.getComputedStyle(document.getElementById('left-hotbar'), "").getPropertyValue("width");
+    
+
 }
 
-adjustHeight();
+adjustSize();
