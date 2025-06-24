4772b1f8157eb31e55d6ae32e9afa3fbaa782438
Julian Linden <DelokrisGaming@web.de>
2025-06-24 09:38:07 +0000

    Fixed height in privacy hotbar

--- js/script.js
+++ js/script.js
@@ -10,3 +10,7 @@ function addToShoppingList(itemID) {
     console.log("Item added to shopping list: " + item);
     console.log("Current shopping list: " + shoppingList.join(", "));
 }
+function adjustSizeImprint(){
+    document.getElementById("left-hotbar").style.height = document.getElementById("container-imprint").getAttribute("height");
+    document.getElementById("left-container").style.height = document.getElementById("container-imprint").getAttribute("height");
+}
