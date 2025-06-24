6bf8b3c772ac43111c88802123633b073a5b84ce
Julian Linden <DelokrisGaming@web.de>
2025-06-24 09:23:45 +0000

    DRY

--- js/script.js
+++ js/script.js
@@ -10,20 +10,44 @@ function addToShoppingList(itemID) {
     console.log("Item added to shopping list: " + item);
     console.log("Current shopping list: " + shoppingList.join(", "));
 }
-function adjustSize() {
+function adjustSizeMain() {
     document.getElementById('left-hotbar').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('left-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('right-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('center-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('home_big').style.width = document.defaultView.getComputedStyle(document.getElementById('home_small'), "").getPropertyValue("width");
     document.getElementById('shop_big').style.width = document.defaultView.getComputedStyle(document.getElementById('shop_small'), "").getPropertyValue("width");
     document.getElementById('about_big').style.width = document.defaultView.getComputedStyle(document.getElementById('about_small'), "").getPropertyValue("width");
+}
+
+function adjustSizeHome() {
+    adjustSizeMain();
+    document.getElementById('right-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    document.getElementById('center-container').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+}
+
+function adjustSizeImprint(){
+    adjustSizeMain();
     document.getElementById('container-impressum').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+    
+}
+
+function adjustSizeContact() {
+    adjustSizeMain();
     document.getElementById('container-contact').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+}
+function adjustSizePrivacy() {
+    adjustSizeMain();
     document.getElementById('container-privacy').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
     document.getElementById('left-hotbar').style.height = document.defaultView.getComputedStyle(document.getElementById('container-privacy'), "").getPropertyValue("height");
-    document.getElementById('container-about').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    document.getElementById('container-shop').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
-    }
+    document.getElementById('left-container').style.height = document.defaultView.getComputedStyle(document.getElementById('container-privacy'), "").getPropertyValue("height");
+}    
+    
 
-adjustSize();
+function adjustSizeAbout() {
+    adjustSizeMain();
+    document.getElementById('container-about').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+}
+
+function adjustSizeShop() {
+    adjustSizeMain();
+    document.getElementById('container-shop').style.top = document.defaultView.getComputedStyle(document.getElementById('header'), "").getPropertyValue("height");
+} 
