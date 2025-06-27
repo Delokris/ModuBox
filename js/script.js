7c9408661455eff52034a93f571a11d1301c2e15
Julian Linden <DelokrisGaming@web.de>
2025-06-27 06:23:18 +0000

    Added change of preset value based on buttonpress

--- js/script.js
+++ js/script.js
@@ -12,7 +12,11 @@ function adjustSizeImprint(){
 
 document.getElementById("buttonBoardgame").onclick = function() {
     document.getElementById("boxLength").value = 30;
+    document.getElementById("boxWidth").value = 30;
+    document.getElementById("boxHeight").value = 8;
     document.getElementById("labelBoxLength").innerHTML = "Länge:<br>" + document.getElementById("boxLength").value + " cm";
+    document.getElementById("labelBoxWidth").innerHTML = "Breite:<br>" + document.getElementById("labelBoxWidth").value + " cm";
+    document.getElementById("labelBoxHeight").innerHTML = "Höhe:<br>" + document.getElementById("labelBoxHeight").value + " cm";
     document.getElementById("configurator").style["z-index"] = "999";
     document.getElementById("configuratorOverlay").style["z-index"] = "1000";
 }
