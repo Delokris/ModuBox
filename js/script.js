4a14e30d097c7633bf08c369f364a646d751f5f2
Julian Linden <DelokrisGaming@web.de>
2025-07-04 06:49:20 +0000

    Added Slideshow to Mainpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 let boxSize = [];
 let shoppingList = [];
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(plusSlides(1), 3000);
+}
 function addToShoppingList(itemID) {
     shoppingList.push(itemID);
     console.log("Item added to shopping list: " + itemID);
